3c365924987daedb93cac885cac5ac4793b8066c
Pedro Lopes <pedro.secinfo@gmail.com>
2025-07-01 21:46:23 +0100

    Fix Railway WebSockets

--- attendance_system_django/attendance/routing.py
+++ attendance_system_django/attendance/routing.py
@@ -4,4 +4,6 @@ from attendance import consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/attendance/class_session_(?P<class_session_id>\w+)/$', consumers.ClassSessionConsumer.as_asgi()),
+    # Add student notifications if needed
+    re_path(r'ws/student/notifications/$', consumers.StudentNotificationConsumer.as_asgi()),
 ]
--- attendance_system_django/attendance_system/asgi.py
+++ attendance_system_django/attendance_system/asgi.py
@@ -3,6 +3,10 @@
 import os
 
 from django.core.asgi import get_asgi_application
+import attendance.routing
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
 
 # IMPORTANT: This line MUST come BEFORE importing any Django models or other
 # code that relies on Django settings being configured.
@@ -24,14 +28,10 @@ import attendance.routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    # Add WebSocket protocol routing
-    "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
-            URLRouter(
-                attendance.routing.websocket_urlpatterns # Include your app's WebSocket URLs
-                # You might include other app's WebSocket URLs here if you have them:
-                # + another_app_routing.websocket_urlpatterns
-            )
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            attendance.routing.websocket_urlpatterns
         )
     ),
 })
+                                 
